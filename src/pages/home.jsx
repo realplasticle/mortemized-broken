0ae0d52375ebe7b126dd4d13f17d4a68218ec7d5
Glitch (glitch-blank-react) <none>
2023-04-27 16:35:17 +0000

    🌻🎋 Checkpoint ./.glitch-assets:51103349/508 ./src/pages/home.jsx:51103349/316 ./src/styles/styles.css:51103349/535

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,18 +3,18 @@ import "../styles/styles.css";
 function CoolBtn({content, disabled, route}){
   
   if (disabled) {
-    return <button className="coolbtn disabled">{content}</button>
+    return <button className="coolshape homebtn disabled">{content}</button>
   } else {
-    return <button ClassName= "coolbtn">{content}</button>
+    return <button ClassName= "coolshape homebtn">{content}</button>
   }
 }
 function MenuBox(){
   return (
-    <>
+    <div className="menubox">
  <CoolBtn content = "new island" disabled = {true} />
 <CoolBtn  content = "load island" disabled = {true}/>
 <CoolBtn content = "import island" disabled = {true}/>
-    </>
+    </div>
   );
 }
 function TitleBox(){
@@ -22,7 +22,7 @@ function TitleBox(){
   <div className="titlebox">
     <img 
       className="titleimg"
-      src= "https://cdn.glitch.global/bf7de285-8738-4504-9358-6f1c68ee7254/2023_04_26_0ka_Kleki.png?v=1682533627908"
+      src= "https://cdn.glitch.global/ffbc945f-5008-4f16-b2ba-4679ba0655a6/35D6CCCD-4699-48A4-B449-2607783D056C.png?v=1682612634645"
       />
       <h1 className="title">mortemized!</h1>
     
