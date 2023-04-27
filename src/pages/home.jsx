ad3ddb0f67d44db1377e74d5a6adf5245867425f
Glitch (glitch-blank-react) <none>
2023-04-27 14:44:24 +0000

    🌺🛬 Checkpoint ./src/pages/home.jsx:51103349/500

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-function CoolBtn(props){
-  let content = props.content;
-  let disabled = props.disabled;
-  let route = props.route;
+function CoolBtn({content, disabled, route}){
   
   if (disabled) {
     return <button className="coolbtn disabled">{content}</button>
@@ -12,12 +9,24 @@ function CoolBtn(props){
 }
 function MenuBox(){
   return (
- <CoolBtn content = "new island" />
-<CoolBtn  content = "load island"/>
-<CoolBtn content = "import island"/>
+    <>
+ <CoolBtn content = "new island" disabled = {true} />
+<CoolBtn  content = "load island" disabled = {true}/>
+<CoolBtn content = "import island" disabled = {true}/>
+    </>
   );
 }
-function TitleBox(){}
+function TitleBox(){
+  return (
+  <div className="title">
+    <img 
+      src= "https://cdn.glitch.global/bf7de285-8738-4504-9358-6f1c68ee7254/2023_04_26_0ka_Kleki.png?v=1682533627908"
+      />
+      
+    
+    </div>
+  )
+}
 function  Menu(){
   
 }
