10a8e2139e528598e996d7627c75fcd5622f322f
Glitch (glitch-blank-react) <none>
2023-04-27 14:03:24 +0000

    💰🔔 Checkpoint ./src/pages/home.jsx:51103349/584

--- src/components/coolbutton.jsx
+++
@@ -1,6 +0,0 @@
-export default function CoolBtn(props){
-  
-  return (
-  <button class="coolbtn">{this.props.content}</button>
-  )
-}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,23 @@
 import * as React from "react";
-
+function CoolBtn(props){
+  let content = props.content;
+  let disabled = props.disabled;
+  let 
+  return (
+    <button className="coolbtn">{content}</button>
+  );
+}
+function MenuBox(){
+  return (
+ <CoolBtn content = "new island" />
+<CoolBtn  content = "load island"/>
+<CoolBtn content = "import island"/>
+  );
+}
+function TitleBox(){}
+function  Menu(){
+  
+}
 
 
 export default function Home() {
