3800c5d147086404b46fffbf4948e3b1a3da2957
realplasticle <113724985+realplasticle@users.noreply.github.com>
2023-05-04 09:37:38 -0500

    update

--- src/pages/new.jsx
+++ src/pages/new.jsx
@@ -1,7 +1,44 @@
 import * as React from "react";
+import { useState } from "react";
 import "../styles/styles.css";
-
-function CoolInput({Placeholder, type}){
-  return (
-  <input type="text" className="coolinput" plac)
+import "../app.jsx";
+function CoolInput({ Placeholder, Type }) {
+  return <input type="text" className="coolinput" placeholder={Placeholder} />;
 }
+function Back() {
+  return (
+    <div className="back">
+      <img
+        className="icon"
+        src="https://cdn.glitch.global/ffbc945f-5008-4f16-b2ba-4679ba0655a6/EF1C100B-11D1-40FC-92C5-D50D11AF7470.png?v=1682614610786"
+      />
+      <p className="backText">back</p>
+    </div>
+  );
+}
+function SelectBtn({ content, value, isSelected, click}) {
+    
+   
+    return (
+ <>
+{ isSelected ? (
+
+ <div className="selectbtn pop" onClick = {click}>{content}</div>
+ 
+ ):(
+   <div className="selectbtn" onClick = {click}>{content}</div>
+    )}
+   
+      </>
+      )
+      }
+
+function Size() {
+let [choice, setChoice] = useState(0);
+  
+  
+  return (
+    <div className="selectbox">
+      <SelectBtn
+        content="a small village and some farmland"
+        value="small"
